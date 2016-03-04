665d41c70de01f33338e5a44c22d77223e92b93c
Jarniel Cataluna <jarnielcataluna@searchoptmedia.com>
2016-03-04 21:08:53 +0800

    updated terms and conditions columns

---
+++ web/mobile/header-mobile.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="robots" content="noindex">
+	<meta name="robots" content="noindex,nofollow"/>
+	<title>Palawan Pawnshop</title>
+
+	<link href="http://pps.searchoptmedia.com/mobile/css/bootstrap.css" rel="stylesheet">
+	<link href="http://pps.searchoptmedia.com/mobile/css/jqueryui.css" rel="stylesheet">
+	
+	<link href="http://pps.searchoptmedia.com/mobile/css/jasny-bootstrap.css" rel="stylesheet">
+	<link href="http://pps.searchoptmedia.com/mobile/css/font-awesome.min.css" rel="stylesheet">
+	<link href="http://pps.searchoptmedia.com/mobile/css/style-mobile.css" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target="#navscroll">
+<div class="nav-wrap">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 desktopNav">
+
+				<nav class="navbar navbar">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						    </button>
+							
+							
+							    <a class="navbar-brand" href="#">
+							    	<div class="logoWrap">
+							    	<img class="logo" src="img/pp-logo.jpg" alt="">
+							    	 </div>
+							    </a>
+						   
+
+						</div>
+
+						<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+
+						<p>CALL:123-4567</p>
+							<ul class="nav navbar-nav nav-pills">
+								<li>
+									<a href="">HOME</a>
+								</li>
+								<li>
+									<a href="">ABOUT US</a>
+								</li>
+								<li class="dropdown">
+									<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SERVICES<span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="/services-pawning-mobile.php">Pawning</a></li>
+										<li><a href="/services-remittance-mobile.php">Pera Padala</a></li>
+										<li><a href="#">Money Changer</a></li>
+										<li><a href="#">Insurance Protektado</a></li>
+										<li><a href="#">Bills Payment</a></li>
+										<li><a href="#">Cash Card/ATM Withdrawal</a></li>
+										<li><a href="#">My Wallet</a></li>
+										<li><a href="#">Suki Card</a></li>
+										<li><a href="#">E-Loading</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="">NEWS/EVENTS</a>
+								</li>
+								<li>
+									<a href="">MEDIA</a>
+								</li>
+								<li>
+									<a href="#">FAQ</a>
+								</li>
+								<li>
+									<a href=""><i class="fa fa-search fa-lg"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+
+			</div>
+
+			<div class="col-md-12 mobileNav">
+
+				<div id="" class="navmenu navmenu-fixed-right offcanvas">
+	                
+		            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+		                    
+		                <div id="sideMenu" class="navbar-default sidebar" role="navigation">
+		                    <div class="sidebar-nav">
+		                        
+	                            <ul class="nav nav-pills" id="side-menu">
+
+	                      			<hr>
+
+	                                <li class="" role="presentation">
+										<a href="">HOME</a>
+									</li>
+									<li>
+										<a href="">ABOUT US</a>
+									</li>
+									<li>
+										<a class="" role="button" data-toggle="collapse" href="#nav1" aria-expanded="false" aria-controls="nav1">SERVICES<span class="caret"></span></a>
+										<div class="collapse" id="nav1">
+											
+											 <ul>
+											 	<li><a href="http://pps.searchoptmedia.com/mobile/services-pawning-mobile.php">Pawning</a></li>
+												<li><a href="http://pps.searchoptmedia.com/mobile/services-remittance-mobile.php">Pera Padala</a></li>
+												<li><a href="#">Money Changer</a></li>
+												<li><a href="#">Insurance Protektado</a></li>
+												<li><a href="#">Bills Payment</a></li>
+												<li><a href="#">Cash Card/ATM Withdrawal</a></li>
+												<li><a href="#">My Wallet</a></li>
+												<li><a href="#">Suki Card</a></li>
+												<li><a href="#">E-Loading</a></li>
+											 </ul>
+											
+										</div>
+
+									</li>
+									<li>
+										<a href="">MEDIA</a>
+									</li>
+									<li>
+										<a href="">FAQ</a>
+									</li>
+
+									 <hr>
+	                            </ul>
+
+	                        </div>
+		                </div>
+		            </nav>
+	            </div>
+
+		        <div class="navbar navbar-default navbar-fixed-top">
+
+		        	<a href=""><img src="img/pp-logo.jpg" class="logo" alt=""></a>
+		        
+									<a href=""><i class="fa fa-search fa-lg"></i></a>
+								
+
+		            <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu" data-canvas="body">
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		            
+		          
+		        </div>
+			</div>
+		</div>
+		</div>
+</div>
+</body>
+</html>
---
+++ web/mobile/index-mobile.php
@@ -0,0 +1,504 @@
+<?php include('header-mobile.php'); ?>
+
+<div class="top-margin"></div>
+<!--
+<div class="row">
+	<div class="col-lg-6">
+		<div class="input-group">
+			<span class="input-group-btn">
+			<button class="btn btn-default" type="button">Go!</button>
+			</span>
+			<input type="text" class="form-control" placeholder="Search for...">
+		</div>
+	</div>
+</div>
+-->
+
+<div class="container-fluid heroCarousel">
+	<div class="row">
+		<!-- Carousel -->
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<!--
+			<ol class="carousel-indicators">
+			  	<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			</ol>
+		-->
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<img class="desktop" src="http://pps.searchoptmedia.com/mobile/img/hero-1-470.jpg" alt="First slide">
+				<img class="mobile" src="http://pps.searchoptmedia.com/mobile/img/hero-1-mobile.jpg" alt="First slide">
+			</div>
+			<div class="item">
+				<img class="desktop" src="http://pps.searchoptmedia.com/mobile/img/hero-2-470.jpg" alt="Second slide">
+				<img class="mobile" src="http://pps.searchoptmedia.com/mobile/img/hero-2-mobile.jpg" alt="First slide">
+			</div>
+			<div class="item">
+				<img class="desktop" src="http://pps.searchoptmedia.com/mobile/img/hero-3-470.jpg" alt="Third slide">
+				<img class="mobile" src="http://pps.searchoptmedia.com/mobile/img/hero-3-mobile.jpg" alt="First slide">
+			</div>
+			<div class="item">
+				<img class="desktop" src="http://pps.searchoptmedia.com/mobile/img/hero-4-470.jpg" alt="Fourth slide">
+				<img class="mobile" src="http://pps.searchoptmedia.com/mobile/img/hero-4-mobile.jpg" alt="First slide">
+			</div>
+		</div>
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		</a>
+		<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div><!-- /carousel -->
+</div>
+</div>
+
+
+
+<div class="container content-filter">
+
+	<div class="row">
+
+		<div class="col-sm-10 col-sm-offset-1 col-xs-12">
+			<div class="content-filter-header">
+				<h1>Ano po ang maipaglilingkod namin sa inyo ? <button class="btn btn-danger toggle-filter">+</button>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row filters">
+		<div class="col-sm-10 col-sm-offset-1">
+			<div class="col-sm-4">
+				<a href="index-pawning.php" class="btn btn-danger btn-lg">MAGSANGLA</a>
+				<p>Pawning</p>
+			</div>
+			<div class="col-sm-4">
+				<a href="index-remittance.php" class="btn btn-danger btn-lg">MAGPADALA</a>
+				<p>Remittance</p>
+			</div>
+			<div class="col-sm-4">
+				<a href="" class="btn btn-danger btn-lg">IBA PA</a>
+				<p>Other Services</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="content-filter-xsMobile">
+	<div class="content-filter-header-xsMobile">
+		<div class="container">
+			<div class="col-sm-10 col-sm-offset-1 col-xs-12">
+				<div class="row">
+					<h1>Ano po ang maipaglilingkod namin sa inyo? <button class="btn btn-danger toggle-filter">—</button> </h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+	<div class="row filters">
+		<div class="col-sm-10 col-sm-offset-1">
+			<div class="col-sm-4">
+				<a href="index-pawning.php" class="btn btn-danger btn-lg">MAGSANGLA</a>
+				<p>Pawning</p>
+			</div>
+			<div class="col-sm-4">
+				<a href="index-remittance.php" class="btn btn-danger btn-lg">MAGPADALA</a>
+				<p>Remittance</p>
+			</div>
+			<div class="col-sm-4">
+				<a href="" class="btn btn-danger btn-lg">IBA PA</a>
+				<p>Other Services</p>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+</div>
+
+<div class="divider">
+	<img src="http://pps.searchoptmedia.com/mobile/img/line-divider.png" alt="">
+</div>
+
+<div class="store-locator">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div class="col-sm-7">
+					<h3>Find A Palawan Pawnshop Near You</h3>
+				</div>
+				<div class="col-sm-3">
+					<div class="form-group">
+						<select class="form-control" id="sel1">
+							<option>Select a location</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-2">
+					<button class="btn btn-danger store-search">SEARCH</button>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="map"></div>
+
+<div class="pawning">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Pawning</h2>
+				<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/pawning-motto.png" alt="">
+			</div>
+			<div class="col-sm-12 description">
+				
+				<p>Palawan Pawnshop has the lowest interest rates in the industry and with high appraisal rates, it continues to be Matatag, Maaasahan and Mapagkakatiwalaan nationwide!</p>
+				<a href="" class="btn btn-danger">HOW TO PAWN</a>
+			</div>
+		</div>
+	</div>
+</div>
+<hr class="borderline-yellow">
+<div class="interest-calculator">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-6">
+					<h3>Lowest interest rate nationwide!</h3>
+					<h4>Try it with our interest calculator.</h4>
+					<p>The interest rate is being computed at 11-day intervals. Customers shall only pay 1% for a loan of 11 days; 2% for 22 days; and 3% for 33 days.</p>
+				</div>
+				<div class="col-md-5 col-md-offset-1 col-sm-8 col-sm-offset-2">
+					<div class="col-md-5  col-md-1 loan-widget">
+						<div class="row">
+							<div class="col-md-12">
+								<label class="title">LOAN</label>
+								<div id="slider-loan-max"></div>
+								<p>
+									<input type="text" id="loanAmount" readonly style="">
+								</p>
+							</div>
+
+							<div class="col-md-12">
+								
+								<label class="title">DURATION</label>
+								<div id="slider-month-max"></div>
+								<p>
+									<input type="text" id="month" readonly style="">
+								</p>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-12">
+								
+								<label class="title">DISCOUNT</label>
+								<label id="suki-radiolabel1" class="radiolabelremit"><input id="radio-suki1" type="radio" name="suki" value="sukiactive"> Suki Card Holder</label>
+								<label id="suki-radiolabel2" class="radiolabelremit"><input id="radio-suki2" type="radio" name="suki" value="nonsuki"> None</label>
+
+							</div>
+						</div>
+
+						<hr>
+
+						<div class="row" id="apply">
+							<div class="col-sm-6 loanValueText">
+								<input type="text" class="form-control" id="loanValue" placeholder="Php 5000.00" readonly>		
+							</div>
+							<div class="col-sm-6 applyButton">
+								<a href="" class="btn btn-danger">LEARN MORE</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+
+<div class="pawning">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Pera Padala</h2>
+				<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/remittance-motto.png" alt="">
+			</div>
+			<div class="col-sm-12">
+				
+				<p> With over 1000 branches nationwide and growing numbers of remittance partners and agents, including LBC and SM Malls, Palawan Express Pera Padala is one of the country's leading money remittance companies today.</p>
+				<a href="" class="btn btn-danger">HOW TO SEND</a>
+			</div>
+		</div>
+	</div>
+</div>
+<hr class="borderline-yellow">
+<div class="padala-calculator">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-6">
+					<h3>Mura Na, Mabilis Pa!</h3>
+					<p>Palawan Express Pera Padala (PEPP) is undeniably the quickest and cheapest way of sending money within the Philippines.</p>
+
+					
+				</div>
+				<div class="col-md-5 col-md-offset-1 col-sm-8 col-sm-offset-2">
+					<div class="col-md-5  col-md-1 loan-widget">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="col-md-6 amountwrap">
+									<div class="form-group">
+										<label for="inputdefault">AMOUNT TO SEND</label>
+										<input name ="defaultInput" class="form-control amount" placeholder="Input Amount" id="inputdefault" type="number" min="1" max="5">
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-sm-12">
+
+								<label class="title">BRANCH</label>
+
+								<div class="col-sm-6">
+									<label id="radio-inline"><input id="radio-vmsl" type="radio" name="locationradio" value="location1">Visayas, Mindano, Southern Luzon</label>
+								</div>
+								<div class="col-sm-6">
+									<label id="radio-inline"><input id="radio-ncrl" type="radio" name="locationradio" value="location2">NCR and the rest of Luzon</label>
+								</div>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-sm-12">
+								<label class="title discountsection" >DISCOUNT</label>
+								<div class="col-sm-6">
+									<label id="suki-radiolabel1" class="radiolabelremit"><input id="radio-suki-remittance1" type="radio" name="remittance" value="sukiremittance"> Suki Card Holder</label>
+									<!-- <label class="radio-inline"><input type="radio" name="optradio">Suki Card Holder</label> -->
+								</div>
+								<div class="col-sm-6">
+									<label id="suki-radiolabel1" class="radiolabelremit"><input id="radio-suki-remittance2" type="radio" name="remittance" value="nonsukiremittance">None</label>
+									<!-- <label class="radio-inline"><input type="radio" name="optradio">None</label> -->
+								</div>
+							</div>
+						</div>
+
+						<hr>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="col-xs-6 loanValueText"><label>REMITTANCE</label>
+									<input class="padalaValue remittance" placeholder="50000" type ="number" readonly>		
+								</div>
+								<div class="col-xs-6 loanValueText"><div class="feelabel">REMITTANCE FEE</div>
+									<input class="feeValue feeholder" placeholder="345" type ="number" readonly>		
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 center">
+				<a href="" class="btn btn-danger text-center">LEARN MORE</a>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+
+<!---
+<div class="bills-payment">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7">
+				<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/bills-payment.png" alt="">
+			</div>
+			<div class="col-sm-5">
+				<h2>Bills Payment</h2>
+				<p>Selected Palawan Pawnshop branches are now accepting payments for your utility bills!</p>
+				<a href="" class="btn btn-danger rightbtn">VIEW PARTNERS</a>
+			</div>
+		</div>
+	</div>
+</div>
+-->
+
+<div class="suki-card">
+	<div class="container">
+		<div class="row ">
+			<div class="col-sm-12">
+				<h2>Suki Card & <br> My Wallet</h2>
+				<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/sukicard.png" alt="">
+			</div>
+			<div class="col-sm-12">
+				<p>Similar to PPS MyWallet card, the PPS PEPP Suki Card will grant the same discounts, special offers and other exciting perks to cardholders. A 5% discount on interest and penalty charges for your pawns and on the corresponding fee for remittance transactions will be accorded to PPS PEPP Suki cardholders.</p>
+				<a href="" class="btn btn-danger howtoapplybtn">APPLY NOW</a>
+				<a href="" class="btn btn-danger learnmorebtn">LEARN MORE</a>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="other-list">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+				<ul>
+					<li><a href="">Bills Payment</a> </li>
+					<li><a href="">Cash Card Withdrawal</a></li>
+					<li><a href="">Money Changer</a></li>
+					<li><a href="">E-loading</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+<!--
+
+<div class="other">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="col-sm-6">
+					<h2>CASH CARD WITHDRAWAL</h2>
+					<p>Palawan Pawnshop Palawan Express Pera Padala’s branches offer BALANCE INQUIRY & WITHDRAWAL transactions thru BDO POS terminals.</p>
+					<a href="" class="btn btn-danger">VIEW BRANCHES</a>
+				</div>
+				<div class="col-sm-6">
+					<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/withdrawal.png" alt="">
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="col-sm-6">
+					<h2>MONEY CHANGER</h2>
+					<p>All Palawan Pawnshop branches accept foreign currencies for exchange to Philippine Peso.</p>
+					<a href="" class="btn btn-danger">EXCHANGE RATES</a>
+				</div>
+				<div class="col-sm-6">
+					<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/money-changer.png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="e-loading">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5">
+				<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/e-loading.png" alt="">
+			</div>
+			<div class="col-sm-7">
+				<h2>E-loading</h2>
+				<p>Palawan Pawnshop branches serve as convenient stations for e-loading for your prepaid mobile subscription! You only have to pay for the exact amount of load, no extra charges!</p>
+				<a href="" class="btn btn-danger rightbtn">VIEW NETWORKS</a>
+			</div>
+		</div>
+	</div>
+</div>
+-->
+<div class="insurance">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Insurance</h2>
+					<img class="img-responsive" src="http://pps.searchoptmedia.com/mobile/img/insurance.png" alt="">
+				
+			</div>
+			<div class="col-sm 12">
+				<p>PPS PEPP ProtekTODO is an accident insurance plan designed to provide Filipino masses social security and personal protection at an affordable option.</p>
+				<a href="" class="btn btn-danger">VIEW BENEFITS</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container testimonials">
+	<div class="row">
+		<h2>Testimonials</h2>
+		<div class="col-sm-12">
+		<!-- Carousel -->
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+				<ol class="carousel-indicators">
+				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			</ol>
+
+
+		
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+		
+					
+						<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde, quis pariatur iure blanditiis beatae
+							<p>-Lorem Ipsum</p>
+			
+				</div>
+				<div class="item">
+					
+				
+						
+						<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde, quis pariatur iure blanditiis beatae
+						<p>-Lorem Ipsum</p>
+				
+				</div>
+				<div class="item">
+					
+				
+						<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et unde, quis pariatur iure blanditiis beatae
+						<p>-Lorem Ipsum</p>
+				
+				</div>
+				</div>
+
+			</div>
+
+
+		</div>
+	</div>
+</div>
+
+<div class="news">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Industry Related News</h2>
+				<div class="col-sm-4">
+					<img src="http://pps.searchoptmedia.com/mobile/img/news-1.jpg" class="img-responsive" alt="">
+					<a href=""><h3>Lorem ipsum dolor sit amet.</h3></a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magnam officiis, quo tenetur, eius dolorum autem blanditiis doloribus quas et.</p>
+				</div>
+				<div class="col-sm-4">
+					<img src="http://pps.searchoptmedia.com/mobile/img/news-1.jpg" class="img-responsive" alt="">
+					<a href=""><h3>Lorem ipsum dolor sit amet.</h3></a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magnam officiis, quo tenetur, eius dolorum autem blanditiis doloribus quas et.</p>
+				</div>
+				<div class="col-sm-4">
+					<img src="http://pps.searchoptmedia.com/mobile/img/news-1.jpg" class="img-responsive" alt="">
+					<a href=""><h3>Lorem ipsum dolor sit amet.</h3></a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magnam officiis, quo tenetur, eius dolorum autem blanditiis doloribus quas et.</p>
+					<a href="" class="btn btn-danger">VIEW ALL NEWS</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php include('footer-mobile.php'); ?>
+
